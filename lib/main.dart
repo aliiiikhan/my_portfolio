2f34d22ce11b039a7ead148537f41c6d9c7a804f
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-11-02 12:38:57 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ali Khan Ak',
-      home: Scaffold(
-        body: Column(),
-      ),
+      home: HomePage()
     );
   }
 }
