4f5de3ac28d5ae222fbc4ac066cbc4eeacc5de27
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-11-11 14:13:11 +0500

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,51 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.green,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.pink,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.yellow,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.red,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
