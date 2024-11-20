becf5641ebe9703306dd512a11e8ee5cceabf78b
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-11-20 12:53:44 +0500

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/nav_items.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +18,10 @@ class _HomePageState extends State<HomePage> {
         children: [
           Container(
             height: 60,
+            margin: EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 20.0,
+            ),
             width: double.maxFinite,
             decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -28,10 +33,13 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Text('AK'),
                 Spacer(),
-                for (int i = 0; i < 5; i++)
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('Button'),
+                for (int i = 0; i < navTitles.length; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(navTitles[i]),
+                    ),
                   ),
               ],
             ),
