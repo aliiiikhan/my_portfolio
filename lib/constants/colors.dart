4b691dd1231f91a37a7f2ea2f98de72a72b03969
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-11-20 12:12:25 +0500

    commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,5 +4,12 @@ import 'package:flutter/material.dart';
 
 class CustomColor {
   static const Color scaffoldBg = Color(0xff252734);
-  static const Color bgLight = Color(0xff333646);
+  static const Color bgLight1 = Color(0xff333646);
+  static const Color bgLight2 = Color(0xff424657);
+  static const Color textFieldbg = Color(0xffC8C9CE);
+  static const Color hintDark = Color(0xff666874);
+  static const Color yellowSecondary = Color(0xff3FFC25C);
+  static const Color yellowPrimary = Color(0xffFFAF29);
+  static const Color whitePrimary = Color(0xffEAEAEB);
+  static const Color whiteSecendory = Color(0xffC8C9CE);
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColor.scaffoldBg,
       body: ListView(
         children: [
           Container(
-            height: 500,
+            height: 60,
             width: double.maxFinite,
-            color: Colors.blueGrey,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, CustomColor.bgLight1],
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Row(
+              children: [
+                Text('AK'),
+                Spacer(),
+                for (int i = 0; i < 5; i++)
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Button'),
+                  ),
+              ],
+            ),
           ),
           SizedBox(
             height: 20,
