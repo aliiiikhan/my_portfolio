9dda823be36033b7b94b0a40abda69cfcc238729
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-11-20 11:28:41 +0500

    commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color scaffoldBg = Color(0xff252734);
+  static const Color bgLight = Color(0xff333646);
+}
